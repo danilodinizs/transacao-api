9cd3fc002a0cf42cd09550fed6e8baec6a06214c
Danilo Diniz Silva <danilo.silva@walsywa.local>
2025-03-06 13:44:32 -0300

    Criado o Service de Transação

--- src/main/java/com/danilo/transacao_api/business/services/TransacaoService.java
+++ src/main/java/com/danilo/transacao_api/business/services/TransacaoService.java
@@ -1,7 +1,11 @@
 package com.danilo.transacao_api.business.services;
 
 import com.danilo.transacao_api.controller.dtos.TransacaoRequestDTO;
+import com.danilo.transacao_api.infrasctructure.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -10,14 +14,25 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransacaoService {
 
+    private static final Logger log = LoggerFactory.getLogger(TransacaoService.class);
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
 
     public void adicionarTransacoes(TransacaoRequestDTO dto) {
-        if(dto.dataHora().isAfter(OffsetDateTime.now())) {
 
+        log.info("Iniciado o processamento de gravar transações.");
+        if(dto.dataHora().isAfter(OffsetDateTime.now())) {
+            log.error("Data e hora maiores que a data e hora atuais");
+            throw new UnprocessableEntity("");
         }
+        if(dto.valor() < 0) {
+            log.error("Valor não pode ser menor que 0");
+            throw new UnprocessableEntity("");
+        }
+
+        listaTransacoes.add(dto)
     }
 
 }
