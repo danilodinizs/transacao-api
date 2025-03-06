63b21e9f5f644e2c5728480b52f3e85d3cbf6412
Danilo Diniz Silva <danilo.silva@walsywa.local>
2025-03-06 15:26:59 -0300

    EstatisticasService finalizado

--- src/main/java/com/danilo/transacao_api/business/services/EstatisticasService.java
+++ src/main/java/com/danilo/transacao_api/business/services/EstatisticasService.java
@@ -9,14 +9,23 @@ import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class EstatisticasService {
 
-    public final TransacaoService transacaoService;
+    public TransacaoService transacaoService;
+
+    public EstatisticasService(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
 
     public EstatisticasResponseDTO calcularEstatisticaseTransacoes(Integer intervaloDeBusca) {
         List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(intervaloDeBusca);
 
-        DoubleSummaryStatistics
+        DoubleSummaryStatistics estatisticasTransacoes = transacoes.stream().mapToDouble(TransacaoRequestDTO::valor).summaryStatistics();
+
+        return new EstatisticasResponseDTO(estatisticasTransacoes.getCount(),
+                estatisticasTransacoes.getSum(),
+                estatisticasTransacoes.getAverage(),
+                estatisticasTransacoes.getMin(),
+                estatisticasTransacoes.getMax());
     }
 }
--- src/main/java/com/danilo/transacao_api/controller/dtos/EstatisticasResponseDTO.java
+++ src/main/java/com/danilo/transacao_api/controller/dtos/EstatisticasResponseDTO.java
@@ -1,4 +1,4 @@
 package com.danilo.transacao_api.controller.dtos;
 
-public record EstatisticasResponseDTO(Integer count, Double sum, Double avg, Double min, Double max) {
+public record EstatisticasResponseDTO(Long count, Double sum, Double avg, Double min, Double max) {
 }
