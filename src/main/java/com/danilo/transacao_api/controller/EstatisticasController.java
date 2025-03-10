575b59001f026bd16aad57388711bd5691ef62b9
Danilo Diniz Silva <danilo.silva@walsywa.local>
2025-03-10 11:52:07 -0300

    API documentada!

--- src/main/java/com/danilo/transacao_api/controller/EstatisticasController.java
+++ src/main/java/com/danilo/transacao_api/controller/EstatisticasController.java
@@ -3,6 +3,9 @@ package com.danilo.transacao_api.controller;
 import com.danilo.transacao_api.business.services.EstatisticasService;
 import com.danilo.transacao_api.business.services.TransacaoService;
 import com.danilo.transacao_api.controller.dtos.EstatisticasResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,12 @@ public class EstatisticasController {
     }
 
     @GetMapping
+    @Operation(description = "Endpoint responsável por buscar estatísticas de transações!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Busca efetuada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Erro na busca!"),
+            @ApiResponse(responseCode = "500", description = "Erro interno de servidor!")
+    })
     public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(@RequestParam(value = "intervaloDeBusca", required = false, defaultValue = "60") Integer intervaloDeBusca) {
         return ResponseEntity.ok(estatisticasService.calcularEstatisticaseTransacoes(intervaloDeBusca));
     }
--- src/main/java/com/danilo/transacao_api/controller/TransacaoController.java
+++ src/main/java/com/danilo/transacao_api/controller/TransacaoController.java
@@ -3,6 +3,9 @@ package com.danilo.transacao_api.controller;
 import com.danilo.transacao_api.business.services.TransacaoService;
 import com.danilo.transacao_api.controller.dtos.TransacaoRequestDTO;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +21,25 @@ public class TransacaoController {
     }
 
     @PostMapping
+    @Operation(description = "Endpoint responsável por adicionar transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação gravada com sucesso!"),
+            @ApiResponse(responseCode = "422", description = "Campos não atendem os requisitos da transação!"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição!"),
+            @ApiResponse(responseCode = "500", description = "Erro interno de servidor!")
+    })
     public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDTO dto) {
         transacaoService.adicionarTransacoes(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsável por deletar transações!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação deletada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição!"),
+            @ApiResponse(responseCode = "500", description = "Erro interno de servidor!")
+    })
     public ResponseEntity<Void> deletarTransacoes() {
         transacaoService.limparTransacoes();
         return ResponseEntity.ok().build();
